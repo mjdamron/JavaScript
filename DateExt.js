5c586fd40d46b2c34119fe8ff1e2439facad1756
Michael Damron <mjdamron@acm.org>
2014-07-03 20:47:16 -0400

    Date object extensions
    Simple functions like AddMinute, ToShortDateString

---
+++ DateExt.js
@@ -0,0 +1,52 @@
+//
+// Date functions taken from 
+// http://stackoverflow.com/questions/563406/add-days-to-datetime-using-javascript
+
+Date.prototype.addDays = function (num) {
+    var value = this.valueOf();
+    value += 86400000 * num;
+    return new Date(value);
+}
+
+Date.prototype.addSeconds = function (num) {
+    var value = this.valueOf();
+    value += 1000 * num;
+    return new Date(value);
+}
+
+Date.prototype.addMinutes = function (num) {
+    var value = this.valueOf();
+    value += 60000 * num;
+    return new Date(value);
+}
+
+Date.prototype.addHours = function (num) {
+    var value = this.valueOf();
+    value += 3600000 * num;
+    return new Date(value);
+}
+
+Date.prototype.addMonths = function (num) {
+    var value = new Date(this.valueOf());
+
+    var mo = this.getMonth();
+    var yr = this.getYear();
+
+    mo = (mo + num) % 12;
+    if (0 > mo) {
+        yr += (this.getMonth() + num - mo - 12) / 12;
+        mo += 12;
+    }
+    else
+        yr += ((this.getMonth() + num - mo) / 12);
+
+    value.setMonth(mo);
+    value.setYear(yr);
+    return value;
+}
+
+// get short date string "MM/DD/YYYY"
+Date.prototype.toShortDateString = function() 
+{
+	return (this.getMonth() + 1) + "/" + this.getDate() + "/" + this.getFullYear();
+}
